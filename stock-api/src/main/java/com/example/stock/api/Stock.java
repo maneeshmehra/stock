a0f717cfe80d12b23b7f366d762a70e0a0dba86e
Maneesh Mehra <maneeshmehra@yahoo.com>
2020-05-04 19:37:53 -0400

    Add capability for returning name, namespace and IP of running pod

--- stock-api/src/main/java/com/example/stock/api/Stock.java
+++ stock-api/src/main/java/com/example/stock/api/Stock.java
@@ -4,10 +4,16 @@ public class Stock {
 
 	private String ticker;
 	private double price;
+	private String podName;
+	private String podNamespace;
+	private String podIP;
 	
-	public Stock(String ticker, double price) {
+	public Stock(String ticker, double price, String podName, String podNamespace, String podIP) {
 		this.ticker = ticker;
 		this.price = price;
+		this.podName = podName;
+		this.podNamespace = podNamespace;
+		this.podIP = podIP;
 	}
 	
 	public String getTicker() {
@@ -24,5 +30,29 @@ public class Stock {
 	
 	public void setPrice(double price) {
 		this.price = price;
+	}
+
+	public String getPodName() {
+		return podName;
+	}
+
+	public void setPodName(String podName) {
+		this.podName = podName;
+	}
+
+	public String getPodNamespace() {
+		return podNamespace;
+	}
+
+	public void setPodNamespace(String podNamespace) {
+		this.podNamespace = podNamespace;
+	}
+
+	public String getPodIP() {
+		return podIP;
+	}
+
+	public void setPodIP(String podIP) {
+		this.podIP = podIP;
 	}
 }
--- stock-api/src/main/java/com/example/stock/api/StockController.java
+++ stock-api/src/main/java/com/example/stock/api/StockController.java
@@ -1,14 +1,24 @@
 package com.example.stock.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
+	
+	@Value("#{environment.POD_NAME}")
+	private String podName;
+	
+	@Value("#{environment.POD_NAMESPACE}")
+	private String podNamespace;
+	
+	@Value("#{environment.POD_IP}")
+	private String podIP;
 
 	@GetMapping("/stock")
 	public Stock greeting(@RequestParam(value = "name", defaultValue = "IBM") String name) {
-		return new Stock(name, 0.00);
+		return new Stock(name, 0.00, podName, podNamespace, podIP);
 	}
 }
